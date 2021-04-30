2924c18468922eb9dab09ac4515280896c62665d
prabaharanbalaji16 <code.development16@gmail.com>
2021-04-30 19:50:30 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,107 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { Controlled as CodeMirror } from 'react-codemirror2';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import 'codemirror/lib/codemirror.css';
+import 'codemirror/theme/material.css';
+
+import 'codemirror/mode/htmlmixed/htmlmixed';
+import 'codemirror/mode/css/css';
+import 'codemirror/mode/javascript/javascript';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      html: '',
+      css: '',
+    };
+  }
+
+  componentDidUpdate() {
+    this.runCode();
+  }
+
+
+  runCode = () => {
+    const { html, css } = this.state;
+
+    const iframe = this.refs.iframe;
+    const document = iframe.contentDocument;
+    const documentContents = `
+      <!DOCTYPE html>
+      <html lang="en">
+      <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible">
+        <title>Document</title>
+        <style>
+          ${css}
+        </style>
+      </head>
+      <body>
+        ${html}
+      </body>
+      </html>
+    `;
+
+    document.open();
+    document.write(documentContents);
+    document.close();
+  };
+
+  render() {
+    const { html, css } = this.state;
+    const codeMirrorOptions = {
+      theme: 'material',
+      lineNumbers: true,
+      scrollbarStyle: null,
+      lineWrapping: true,
+    };
+
+    return (
+      <div className="App">
+        <div className="top"></div>
+        <section className="playground">
+           <div className="code-editor html-code">
+           <di className="editor-header">HTML</di>
+           <CodeMirror
+             value={html}
+             options={{
+               mode: 'htmlmixed',
+               theme:'dracula',
+               lineNumbers:true,
+             }}
+             onBeforeChange={(editor, data, html) => {
+               this.setState({ html });
+             }}
+           />
+         </div> 
+         <div className="code-editor css-code">
+         <div className="editor-header">CSS</div>
+         <CodeMirror
+           value={css}
+           options={{
+             mode: 'css',
+             lineNumbers:true
+           }}
+           onBeforeChange={(editor, data, css) => {
+             this.setState({ css });
+           }}
+         />
+       </div> 
+          
+        </section>
+        <section className="result">
+        <div className="editor-header">Live Preview
+           
+        </div>
+          <iframe title="result" className="iframe" ref="iframe" />
+        </section>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCKFJucAM2Aw7XaC5DzvoUr6u0CC7smZTs",
+    authDomain: "ide-3042e.firebaseapp.com",
+    projectId: "ide-3042e",
+    storageBucket: "ide-3042e.appspot.com",
+    messagingSenderId: "198919064271",
+    appId: "1:198919064271:web:048c5ecf25ac17591585ba",
+    measurementId: "G-721K9YQQKF"
+  };
